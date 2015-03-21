481e27bb9699aa736bd9d6e6a2efd2ce88352e1e
DilupaNadi <dila.june@ymail.com>
2015-03-21 14:22:54 +0530

    hj

--- application/views/file_type/manage_file_type_view.php
+++ application/views/file_type/manage_file_type_view.php
@@ -492,6 +492,8 @@ Use readAsBinaryString: (when available) <input type="checkbox" name="userabs" c
 
 <style>.tb_button {padding:1px;cursor:pointer;border-right: 1px solid #8b8b8b;border-left: 1px solid #FFF;border-bottom: 1px solid #fff;}.tb_button.hover {borer:2px outset #def; background-color: #f8f8f8 !important;}.ws_toolbar {z-index:100000} .ws_toolbar .ws_tb_btn {cursor:pointer;border:1px solid #555;padding:3px}   .tb_highlight{background-color:yellow} .tb_hide {visibility:hidden} .ws_toolbar img {padding:2px;margin:0px}</style></body></html>
 
+
+
 <div class="row-fluid">
     <div class="span12">
         <div class="grid simple ">
@@ -543,13 +545,13 @@ Use readAsBinaryString: (when available) <input type="checkbox" name="userabs" c
 
                             <div id="add_file_type_msg" class="form-row"> </div>
 
-                            <div class="modal-footer">
-                                <button class="btn btn-primary btn-cons" type="submit">
-                                    <i class="icon-ok"></i>
-                                    Save
-                                </button>
-                                <a href="<?php echo site_url(); ?>/file_type/file_type_controller/manage_upload_files_stuff" class="btn btn-white btn-cons" type="button">Cancel</a>
-                            </div>
+                            <!--                            <div class="modal-footer">
+                                                            <button class="btn btn-primary btn-cons" type="submit">
+                                                                <i class="icon-ok"></i>
+                                                                Save
+                                                            </button>
+                                                            <a href="<?php echo site_url(); ?>/file_type/file_type_controller/manage_upload_files_stuff" class="btn btn-white btn-cons" type="button">Cancel</a>
+                                                        </div>-->
 
                         </form>
                     </div>
